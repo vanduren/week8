107d374b152b644e499d5a0579db165243a5bb0d
=soplar <=joan.vanduren@gmail.com>
2022-11-10 15:35:03 +0100

    github conrainer toegevoegd

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,17 +1,26 @@
-Maak nieuw melkproduct:
-<form action="{{ route('product.create') }}" method="GET">
-    <input type="submit" value="maak product">
-</form>
+@extends('layouts.app')
 
-@foreach ($products as $product)
-    <a href="{{ route('product.show', $product) }}">
-        {{ $product->name }}
-    </a>
-    {{ $product->price/100 }}
-    <form action="{{ route('product.destroy', $product) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <input type="submit" value="verwijder">
+@section('content')
+<div>
+    Maak nieuw melkproduct:
+    <form action="{{ route('product.create') }}" method="GET">
+        <div>
+        <input type="submit" value="maak product" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        </div>
     </form>
-    <br>
-@endforeach
+</div>
+    @foreach ($products as $product)
+        <div class="bg-blue-100 hover:bg-blue-700">
+        <a href="{{ route('product.show', $product) }}">
+            {{ $product->name }}
+        </a>
+        {{ $product->price/100 }}
+        <form action="{{ route('product.destroy', $product) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="verwijder">
+        </form>
+        <br>
+        </div>
+    @endforeach
+@endsection
